7b009db804eed03fb37f50e4573fec87f507a19e
Sid <albertaodasmassa@gmail.com>
2024-11-05 17:19:15 -0300

    Update PlayerCollect.cs

--- Scripts_jogo/PlayerCollect.cs
+++ Scripts_jogo/PlayerCollect.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class PlayerCollect : MonoBehaviour
 {
     public int score = 0;
+    public AudioClip collectSound; // Som de coleta
+    private AudioSource audioSource;
+    public ParticleSystem collectEffect;
+    public ParticleSystem copia;
+    private void Start()
+    {
+        // Obtém o componente de áudio
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit){
+        
+        Instantiate(collectEffect, hit.transform.position, Quaternion.identity);
+        copia = Instantiate(collectEffect, hit.transform.position, Quaternion.identity);
+        copia.gameObject.SetActive(true);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Coletavel"))
+        if(other.CompareTag("Coletável"))
         {
             score +=10;
+            audioSource.PlayOneShot(collectSound);
             Destroy(other.gameObject);
-            Debug.Log("Coletavel Obtido! Pontuação:"+score);
+            Debug.Log("Coletável obtido! Pontuação: " + score);
+            audioSource.PlayOneShot(collectSound);
         }
     }
 }
+
