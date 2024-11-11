54923c6ab9ae3344a3966335275d954e25c7bd60
Sid <albertaodasmassa@gmail.com>
2024-11-11 15:27:26 -0300

    Update PlayerMovement.cs

--- Scripts_jogo/PlayerMovement.cs
+++ Scripts_jogo/PlayerMovement.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(CharacterController))] // Assegura que o CharacterController está presente
+public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 6.0f;
-    private CharacterController controller;
+    public float speed = 6.0f; // Velocidade de movimento do Player
+    
+    private CharacterController controller; // Referência ao CharacterController
+
+    
+    //O método Start() é chamado UMA VEZ assim que o jogo começa
     void Start()
     {
+        
+        // Obtém o componente CharacterController que está anexado ao Player
+        //GetComponent significa "Pegar componente"
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // Obter entrada do usuário (teclas W, A, S, D)
+        float horizontalInput = Input.GetAxis("Horizontal"); // Entrada para esquerda e direita
+        float verticalInput = Input.GetAxis("Vertical");     // Entrada para frente e trás
+
+        // Cria um vetor de direção com base na entrada
         Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput;
-        controller.SimpleMove(moveDirection*speed);
-        
+
+        // Move o Player com o CharacterController
+        controller.SimpleMove(moveDirection * speed);
     }
 }
