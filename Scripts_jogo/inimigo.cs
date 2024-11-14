42bf0a398f863e2a24a989af9ff6ca222f5f60b8
Sid <albertaodasmassa@gmail.com>
2024-11-14 14:02:25 -0300

    Update inimigo.cs

--- Scripts_jogo/inimigo.cs
+++ Scripts_jogo/inimigo.cs
@@ -8,11 +8,17 @@ public class Inimigo : MonoBehaviour
     public int Health; // Vida máxima
     public ParticleSystem sm_ini; // Efeito visual de coleta
     public ParticleSystem copia;
+    public AudioClip collectSound; // Som de coleta
+    private AudioSource audioSource; 
+    public GameObject player;
+    private PlayerStats playerStats;
 
    // Método para atualizar a vida
     void Start()
     {
         Health=100;
+        audioSource = gameObject.AddComponent<AudioSource>();
+         playerStats = player.GetComponent<PlayerStats>();
     }
 
     public void TakeDamage(int damage)
@@ -24,9 +30,11 @@ public class Inimigo : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Projétil"))
         {
-        TakeDamage(5);
+        TakeDamage(50);
         Destroy(other.gameObject);
         if (Health <= 0){
+            audioSource.PlayOneShot(collectSound);
+            playerStats.AddScore(10);
             Destroy(this.gameObject);
             copia = Instantiate(sm_ini, this.transform.position, Quaternion.identity);
         }
