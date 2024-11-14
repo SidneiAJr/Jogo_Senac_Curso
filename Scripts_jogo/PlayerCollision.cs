5dfef79a7309d5b8c198b80e38d012997193bbb2
Sid <albertaodasmassa@gmail.com>
2024-11-14 17:03:30 -0300

    Update PlayerCollision.cs

--- Scripts_jogo/PlayerCollision.cs
+++ Scripts_jogo/PlayerCollision.cs
@@ -5,34 +5,41 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public AudioClip collectSound; // Som de coleta
-    public ParticleSystem collectEffect; // Efeito visual de coleta
     private AudioSource audioSource;
-    public ParticleSystem copia;
+    public ParticleSystem collectEffect; // Efeito visual de coleta
+    private ParticleSystem copia; // Cópia da partícula
 
     private void Start()
     {
         // Obtém o componente de áudio
         audioSource = gameObject.AddComponent<AudioSource>();
     }
+
     // Método chamado automaticamente quando o jogador colide com outro objeto
-   private void OnControllerColliderHit(ControllerColliderHit hit)
-   {
-    // Verifica se o objeto com o qual colidimos possui a tag "Collectible"
-    if(hit.gameObject.CompareTag("item"))
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Debug.Log("Item Coletado com sucesso");
-        // Exibe uma mensagem no console indicando a colisão
-        // Aqui você pode adicionar outras interações, como efeitos sonoros, pontos, etc.
-        // Desativa o item coletado
-        audioSource.PlayOneShot(collectSound);
-        
-        Instantiate(collectEffect, hit.transform.position, Quaternion.identity);
-        copia = Instantiate(collectEffect, hit.transform.position, Quaternion.identity);
-        copia.gameObject.SetActive(true);
+        // Verifica se o objeto com o qual colidimos possui a tag "Item"
+        //SE o GameObject com o qual o player colidir tiver a tag "Item", acontece algo
+        if (hit.gameObject.CompareTag("item"))
+        {
+            // Exibe uma mensagem no console indicando a colisão
+            Debug.Log("Item coletado!");
+            
+            // Aqui você pode adicionar outras interações, como efeitos sonoros, pontos, etc.
+            
+             // Toca o som de coleta
+            audioSource.PlayOneShot(collectSound);
 
-        hit.gameObject.SetActive(false);
+            // Ativa o efeito de partículas na posição do objeto coletado
+            //Instantiate: método que cria uma cópia de um objeto no jogo. Os argumentos são: 1- o objeto que eu quero instanciar, 2- a posição onde ele será criado, 3 - a rotação dele
+            //Quaternion é o comando que usamos na Unity para pegar a rotação de algo. O identity indica que a rotação é neutra, sem rotação.
+            copia = Instantiate(collectEffect, hit.transform.position, Quaternion.identity);
 
+            copia.gameObject.SetActive(true);
 
+            // Desativa o item coletado
+            hit.gameObject.SetActive(false);
+            
+        }
     }
-   }
 }
