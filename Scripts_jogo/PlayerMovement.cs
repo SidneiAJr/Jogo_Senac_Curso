b3f6b18955bc32b75d798b5aa8f02f948b96e1f0
Sid <albertaodasmassa@gmail.com>
2024-11-14 20:45:08 -0300

    Update PlayerMovement.cs

--- Scripts_jogo/PlayerMovement.cs
+++ Scripts_jogo/PlayerMovement.cs
@@ -36,6 +36,21 @@ public class PlayerMovement : MonoBehaviour
         {
             playerVelocity.y = 0f;
         }
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Makes the player jump
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
 
         // Cria um vetor de direção com base na entrada
         Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput;
