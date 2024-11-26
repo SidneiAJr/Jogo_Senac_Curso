f8d3bb8cda6efbe0ccb3b564d5835e718963eef3
Sid <albertaodasmassa@gmail.com>
2024-11-26 19:27:19 -0300

    Update dano.cs

--- Scripts_jogo/dano.cs
+++ Scripts_jogo/dano.cs
@@ -10,11 +10,9 @@ public class Coleta : MonoBehaviour
     public AudioClip collectSound; // Som de coleta
     private AudioSource audioSource;
     private PlayerStats playerStats; // Referência ao PlayerStats
-    public int Armor;
-    private int dano;
+    private float dano;
     private void Start()
     {
-         Armor = 10;
          dano = 10;
         // Obtém o componente de áudio
         audioSource = gameObject.AddComponent<AudioSource>();
@@ -27,14 +25,10 @@ public class Coleta : MonoBehaviour
         if (other.CompareTag("armor"))
         {
             //score += 10;  // Aumenta a pontuação
-             Armor += 20;
-             dano +=10;
+             dano +=7.5;
 
             // Adiciona pontos ao chamar o método AddScore de PlayerStats; // Adiciona 10 pontos (pode ajustar o valor como preferir)
             playerStats.Addarmor(10);
-             playerStats.Addarmor(10);
-            
-
             audioSource.PlayOneShot(collectSound);
             Destroy(other.gameObject);  // Remove o coletável
             Debug.Log("Armadura Coletada " +  Armor);
