981c3133a17483db287b3076785d50333e62e0b4
Sid <albertaodasmassa@gmail.com>
2024-11-26 16:55:00 -0300

    Create ArmorCollect.cs

---
+++ Scripts_jogo/ArmorCollect.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorCollect : MonoBehaviour
+{
+    //public int score = 0;  // Armazena a pontuação do jogador
+
+    // Detecta colisão com o coletável
+    public AudioClip collectSound; // Som de coleta
+    private AudioSource audioSource;
+    private PlayerStats playerStats; // Referência ao PlayerStats
+    private int armor;
+    private void Start()
+    {
+        armor = 10;
+        // Obtém o componente de áudio
+        audioSource = gameObject.AddComponent<AudioSource>();
+         // Busca o componente PlayerStats no mesmo GameObject
+        playerStats = GetComponent<PlayerStats>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("armor"))
+        {
+            //score += 10;  // Aumenta a pontuação
+            armor += 20;
+
+            // Adiciona pontos ao chamar o método AddScore de PlayerStats; // Adiciona 10 pontos (pode ajustar o valor como preferir)
+            playerStats.Addarmor(10);
+
+            audioSource.PlayOneShot(collectSound);
+            Destroy(other.gameObject);  // Remove o coletável
+            Debug.Log("Armadura Coletada " + armor);
+        }
+    }
+}
