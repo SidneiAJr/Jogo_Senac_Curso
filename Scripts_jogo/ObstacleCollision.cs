2f04ef6a0bab708462a3b8c522a4069dfe802650
Sid <albertaodasmassa@gmail.com>
2024-12-02 21:11:17 -0300

    Update ObstacleCollision.cs

--- Scripts_jogo/ObstacleCollision.cs
+++ Scripts_jogo/ObstacleCollision.cs
@@ -2,20 +2,21 @@ using UnityEngine;
 
 public class ObstacleCollision : MonoBehaviour
 {
-    //public int health = 100;  // Vida do jogador
-    private PlayerStats playerStats; // Referência ao PlayerStats -----------------------
+    private  PlayerStats playStats;
+    private  PlayerStats maxHealth;
+    private PlayerStats playerStats; // Referência ao PlayerStats 
 
-    private void Start() { // -----------------------
-        playerStats = GetComponent<PlayerStats>(); // -----------------------
-    } // -----------------------
+    private void Start() { 
+        playerStats = GetComponent<PlayerStats>(); 
+    } 
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.gameObject.CompareTag("Obstáculo"))
         {
-            playerStats.TakeDamage(1); // -----------------------
-            //health -= 20;  // Reduz a vida
-            //Debug.Log("Colidiu com obstáculo! Vida: " + health);
+            playerStats.TakeDamage(1); 
+            playerStats.maxHealth -= 1;
+            Debug.Log("Voce foi Atigindo" + maxHealth);
         }
     }
 }
