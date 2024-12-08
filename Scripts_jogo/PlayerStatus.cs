152d33995313c0d36bbbe26dc1bb4ee3ed14cc6d
Sid <albertaodasmassa@gmail.com>
2024-12-07 23:52:12 -0300

    Update PlayerStatus.cs

--- Scripts_jogo/PlayerStatus.cs
+++ Scripts_jogo/PlayerStatus.cs
@@ -6,12 +6,12 @@ using TMPro;
 
 public class PlayerStatus : MonoBehaviour
 {
-    public int nivel = 1;                  // Nível atual do jogador
+    public int nivel = 0;                  // Nível atual do jogador
     public int vidaAtual;                 // Vida atual do jogador
     public int vidaMaxima = 100;         // Vida máxima do jogador
     public int experienciaAtual = 0;     // Experiência atual do jogador
     public int experienciaParaSubir = 100; // Quantidade de experiência necessária para subir de nível
-    public int aumentoVidaPorNivel = 20;   // Quanto a vida aumenta por nível
+    public int aumentoVidaPorNivel = 30;   // Quanto a vida aumenta por nível
     public TMP_Text Xpplayer;
     public TMP_Text VidaPlayer;
     private Text nivelText;
@@ -19,6 +19,12 @@ public class PlayerStatus : MonoBehaviour
     public int experiencia = 0;
     public int experienciaParaProximoNivel = 5; // Quantidade de XP necessária para o próximo nível
     public int experienciaPorNivel = 5; // A XP necessária por nível
+    private int armaduraplayer = 5;
+    private int velataque = 5;
+    private double roubo = 0.25;
+    public int armaduraplyernivel = 30;
+    public int roubonivel = 30;
+    public int velataquenivel = 20;
 
 
     public void AdicionarExperiencia(int quantidade)
@@ -42,7 +48,9 @@ public class PlayerStatus : MonoBehaviour
         
         // Aumenta a vida máxima
         vidaMaxima += aumentoVidaPorNivel;
-        
+        armaduraplayer += armaduraplyernivel;
+        roubo += roubonivel;
+        velataque += velataquenivel;
         // Restaura a vida do jogador para o valor máximo
         vidaAtual = vidaMaxima;
         Debug.Log("Vida aumentada! Vida Máxima: " + vidaMaxima);
